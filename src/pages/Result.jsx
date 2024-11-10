a2579970ab25d3afe8148de33fb6f1fd17951fc1
Luke Wang <lmwang@usc.edu>
2024-11-10 14:10:06 -0800

    updated results

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -6,13 +6,11 @@ const ResultBox = ({ tripPlan }) => {
     return <div>Loading trip plan...</div>;  // or handle the absence of tripPlan in a different way
   }
 
-  // Assuming tripPlan is an object with some properties to display
   return (
     <div className="result-box">
       <h2>Your Trip Plan</h2>
-      <ul>
-        <li> {tripPlan}</li>
-      </ul>
+      {/* Use dangerouslySetInnerHTML to inject HTML content */}
+      <div dangerouslySetInnerHTML={{ __html: tripPlan }} />
     </div>
   );
 };
